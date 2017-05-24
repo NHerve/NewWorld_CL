5949a17179f10f385636d3a86003d3fc4bdd8173
NHerve <herve.04@hotmail.fr>
2017-05-24 15:03:14 +0200

    Add files via upload

---
+++ MDB/connexion.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>NewWorld</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+
+    <!--Navbar-->
+    <nav class="navbar navbar-toggleable-md navbar-dark scrolling-navbar fixed-top">
+        <div class="container">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapseEx2" aria-controls="collapseEx2" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">
+                <strong>NW</strong>
+            </a>
+            <div class="collapse navbar-collapse" id="collapseEx2">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link">Acheter<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Produire</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Distribuer</a>
+                    </li>
+                </ul>
+                <form class="form-inline waves-effect waves-light">
+                    <input class="form-control" type="text" placeholder="Search">
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!--/.Navbar-->
+
+    <!--Mask-->
+    <div class="view hm-black-strong" id="monIdMenu">
+        <div class="full-bg-img flex-center">
+            <ul>
+                <li>
+                    <h1 class="h1-responsive wow fadeInDown" data-wow-delay="0.2s">Connexion</h1></li>
+                <li>	
+			<form method="post" class="formulaire_connexion" action="fonctionPhp/connexionFonction.php">
+		<div class="row">
+
+			<div class="col-md-12">
+	
+				<div class="md-form">
+      					 <input type="text" name="Login" class="form-control wow bounceIn" data-wow-delay="0.2s" required>
+   					 <label class=" wow bounceIn" data-wow-delay="0.2s" for="form2">Login*</label>
+				</div>
+
+				<div class="md-form">
+      					 <input type="Password" name="Password" class="form-control wow bounceIn" data-wow-delay="0.2s" required>
+   					 <label class=" wow bounceIn" data-wow-delay="0.2s" for="form2">Mot de passe*</label>
+				</div>
+
+				</br></br>
+
+			
+				<input class="btn btn-primary wow fadeInDown" data-wow-delay="0.2s" type="submit" value="Connexion">			
+			
+			</div>
+
+		    </form>
+		</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--/.Mask-->
+
+   <!--Footer-->
+    <footer class="page-footer center-on-small-only mdb-color darken-4">
+        <hr>
+
+        <!--Call to action-->
+        <div class="call-to-action">
+            <h4>NewWorld</h4>
+            <ul>
+                <li>
+                    <h5>Connecter vous des maintenant</h5></li></br>
+                <li><a target="_blank" href="https://mdbootstrap.com/material-design-for-bootstrap/" class="btn btn-default">Connexion</a></li>
+            </ul>
+        </div>
+        <!--/.Call to action-->
+
+    </footer>
+    <!--/.Footer-->
+
+
+    <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/tether.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+
+    <!--Google Maps-->
+    <script src="https://maps.google.com/maps/api/js"></script>
+
+    <!-- Animations init-->
+    <script>
+        new WOW().init();
+    </script>
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/tether.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+		
+
+</body>
+
+</html>
---
+++ MDB/index.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>NewWorld</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+
+    <!--Navbar-->
+    <nav class="navbar navbar-toggleable-md navbar-dark scrolling-navbar fixed-top">
+        <div class="container">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapseEx2" aria-controls="collapseEx2" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <strong>NW</strong>
+            </a>
+            <div class="collapse navbar-collapse" id="collapseEx2">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link">Acheter<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Produire</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Distribuer</a>
+                    </li>
+                </ul>
+                <form class="form-inline waves-effect waves-light">
+                    <input class="form-control" type="text" placeholder="Search">
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!--/.Navbar-->
+
+    <!--Mask-->
+    <div class="view hm-black-strong" id="monIdMenu">
+        <div class="full-bg-img flex-center">
+            <ul>
+                <li>
+                    <h1 class="h1-responsive wow fadeInDown" data-wow-delay="0.2s">NewWorld</h1></li>
+                <li>
+                    <p class="wow fadeInDown">Les meilleurs produits de saison.
+Du producteur a l'artisan et au consommateur.</br>
+Ni usine, ni camion, ni grande surface.
+La terre et l'homme a nouveau respectes
+</p>
+                </li>
+                <li>
+                    <a href="inscription.php" class="btn btn-primary btn-lg wow fadeInLeft" data-wow-delay="0.2s">Inscription</a>
+                    <a href="connexion.php" class="btn btn-default btn-lg wow fadeInRight" data-wow-delay="0.2s">Connexion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--/.Mask-->
+
+    <!-- Main container-->
+    <div class="container">
+
+        <div class="divider-new">
+            <h2 class="h2-responsive wow bounceIn">Info</h2>
+        </div>
+
+        <!--Section: Info-->
+        <section id="about" class="text-center wow bounceIn" data-wow-delay="0.2s">
+
+            <p><h5> Producteur</h5>
+Agriculteurs, éleveurs
+Vous souhaitez proposer au juste prix des produits de qualité, sains, frais et de saison.</p>
+
+            <p><h5>Adulte éco-responsable</h5>
+
+Vous êtes un père ou une mère de famille responsable qui sait non seulement que l'on doit manger sain mais aussi que pour maintenir un centre ville et des campagnes peuplés, il est nécessaire de donner leurs chances aux producteurs et artisants.Les emplois que vous maintenez aujourd'hui seront peut-être ceux de vos enfants. Il faut développer l'activité locale en réduisant les coûts de transport ainsi que les intermédiaires. Vous souhaitez proposer au juste prix des produits de qualité, sains, frais et de saison.</p>
+
+	    <p><h5>Artisan de l'alimentaire</h5>
+Vous transformez les produits frais locaux et souhaitez maintenir votre commerce de centre ville face à l'omniprésence des grandes surfaces. Vous voyez chaque jour autour de vous des magasins qui ferment. NewWorld peut vous permettre un complément d'activité. Tentez cela ne coûte rien.</p>
+
+        </section>
+        <!--Section: Info-->
+
+        <div class="divider-new">
+            <h2 class="h2-responsive wow fadeInDown">Principes</h2>
+        </div>
+
+        <!--Section: Best features-->
+        <section id="best-features">
+
+            <div class="row">
+                <!--First columnn-->
+                <div class="col-md-4">
+                    <!--Card-->
+                    <div class="card wow fadeInUp">
+
+                        <!--Card image-->
+                        <div class="view overlay hm-white-slight">
+                            <img src="img/img/boucher2" class="img-fluid" alt="">
+                            <a>
+                                <div class="mask"></div>
+                            </a>
+                        </div>
+                        <!--/.Card image-->
+
+                        <!--Card content-->
+                        <div class="card-block text-center">
+                            <!--Title-->
+                            <h4 class="card-title">Participer</h4>
+                            <hr>
+                            <!--Text-->
+                            	<p class="card-text">Proposer des produits
+Transformer</br>
+Devenir client</br>
+Distribue</br>
+				</p>
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
+                </div>
+                <!--First columnn-->
+
+                <!--Second columnn-->
+                <div class="col-md-4">
+                    <!--Card-->
+                    <div class="card wow fadeInUp" data-wow-delay="0.2s">
+
+                        <!--Card image-->
+                        <div class="view overlay hm-white-slight">
+                            <img src="img/img/jardinier2" class="img-fluid" alt="">
+                            <a>
+                                <div class="mask"></div>
+                            </a>
+                        </div>
+                        <!--/.Card image-->
+
+                        <!--Card content-->
+                        <div class="card-block text-center">
+                            <!--Title-->
+                            <h4 class="card-title">Comprendre</h4>
+                            <hr>
+                            <!--Text-->
+				<p class="card-text">
+    Savoir faire local</br>
+    Réduire les transports</br>
+    Produits frais</br>
+    Qualité</br>
+                            	</p>
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
+                </div>
+                <!--Second columnn-->
+
+                <!--Third columnn-->
+                <div class="col-md-4">
+                    <!--Card-->
+                    <div class="card wow fadeInUp" data-wow-delay="0.4s">
+
+                        <!--Card image-->
+                        <div class="view overlay hm-white-slight">
+                            <img src="img/img/repas.jpg" class="img-fluid" alt="">
+                            <a>
+                                <div class="mask"></div>
+                            </a>
+                        </div>
+                        <!--/.Card image-->
+
+                        <!--Card content-->
+                        <div class="card-block text-center">
+                            <!--Title-->
+                            <h4 class="card-title">Commuiquer</h4>
+                            <hr>
+                            <!--Text-->
+				<p class="card-text">
+    Les secrets des producteurs</br>
+    Le savoir faire des artisants</br>
+    Les recettes de grand-mère</br>
+    La conservation des aliments</br>
+                           	</p>
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
+                </div>
+                <!--Third columnn-->
+            </div>
+
+        </section>
+        <!--/Section: Best features-->
+
+        <div class="divider-new">
+            <h2 class="h2-responsive wow fadeInUp">Contact</h2>
+        </div>
+
+        <!--Section: Contact-->
+        <section id="contact">
+            <div class="row">
+                <!--First column-->
+                <div class="col-md-8">
+                    <div id="map-container" class="z-depth-1 wow fadeInUp" style="height: 300px"></div>
+                </div>
+                <!--/First column-->
+
+                <!--Second column-->
+                <div class="col-md-4">
+                    <ul class="text-center">
+                        <li class="wow fadeInUp" data-wow-delay="0.2s"><i class="fa fa-map-marker"></i>
+                            <p>Place Verdun 05000 GAP</p>
+                        </li>
+
+                        <li class="wow fadeInUp" data-wow-delay="0.3s"><i class="fa fa-phone"></i>
+                            <p>04 05 06 07 08</p>
+                        </li>
+
+                        <li class="wow fadeInUp" data-wow-delay="0.4s"><i class="fa fa-envelope"></i>
+                            <p>NewWorld@NW.com</p>
+                        </li>
+                    </ul>
+                </div>
+                <!--/Second column-->
+            </div>
+        </section>
+        <!--Section: Contact-->
+
+    </div>
+    <!--/ Main container-->
+
+
+
+    <!--Footer-->
+    <footer class="page-footer center-on-small-only mdb-color darken-4">
+        <hr>
+
+        <!--Call to action-->
+        <div class="call-to-action">
+            <h4>NewWorld</h4>
+            <ul>
+                <li>
+                    <h5>Inscrivez vous des maintenant</h5></li></br>
+                <li><a href="inscription.php"  class="btn btn-danger">Inscription</a></li>
+                <li><a target="_blank" href="https://mdbootstrap.com/material-design-for-bootstrap/" class="btn btn-default">Connexion</a></li>
+            </ul>
+        </div>
+        <!--/.Call to action-->
+
+    </footer>
+    <!--/.Footer-->
+
+
+    <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/tether.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+
+    <!--Google Maps-->
+    <script src="https://maps.google.com/maps/api/js"></script>
+
+    <script>
+        function init_map() {
+
+            var var_location = new google.maps.LatLng(44.561199, 6.075911);
+
+            var var_mapoptions = {
+                center: var_location,
+
+                zoom: 14
+            };
+
+            var var_marker = new google.maps.Marker({
+                position: var_location,
+                map: var_map,
+                title: "New York"
+            });
+
+            var var_map = new google.maps.Map(document.getElementById("map-container"),
+                var_mapoptions);
+
+            var_marker.setMap(var_map);
+
+        }
+
+        google.maps.event.addDomListener(window, 'load', init_map);
+    </script>
+
+    <!-- Animations init-->
+    <script>
+        new WOW().init();
+    </script>
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/tether.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+		
+
+</body>
+
+</html>
